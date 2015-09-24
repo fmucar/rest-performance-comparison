59266e2339a5f98a86a8cf8cc87eda380762da59
fucar <fmucar@cooldatasoft.com>
2015-09-24 20:52:49 +0100

    adding tomcat-jersey

---
+++ tomcat-jersey/src/main/java/com/cooldatasoft/rest/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.cooldatasoft.rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring configuration via annotations
+ * @author Fatih
+ *
+ */
+
+@Configuration
+@ComponentScan("com.cooldatasoft.rest")
+public class AppConfig {
+
+}
---
+++ tomcat-jersey/src/main/java/com/cooldatasoft/rest/resource/impl/SampleResourceImpl.java
@@ -0,0 +1,63 @@
+package com.cooldatasoft.rest.resource.impl;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.cooldatasoft.rest.data.SampleEntity;
+import com.cooldatasoft.rest.service.SampleService;
+
+@Named
+@Path("/sample")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class SampleResourceImpl {
+
+	@Inject
+	private SampleService sampleService;
+
+	@POST
+	@Path("/create")
+	public Response create(SampleEntity entity) {
+		return Response.ok(sampleService.create()).build();
+	}
+
+	@GET
+	@Path("/read/{id}")
+	public Response read(@PathParam("id") Long id) {
+		return Response.ok(sampleService.read(id)).build();
+	}
+
+	@PUT
+	@Path("/update")
+	public Response update(SampleEntity entity) {
+		return Response.ok(sampleService.update(entity)).build();
+	}
+
+	@DELETE
+	@Path("/delete/{id}")
+	public Response delete(@PathParam("id") Long id) {
+		return Response.ok(sampleService.delete(id)).build();
+	}
+
+	@GET
+	@Path("/list")
+	public Response list(@QueryParam("startId") @DefaultValue("1") Long startId, @QueryParam("size") @DefaultValue("20") Integer size) {
+		List<SampleEntity> entities = sampleService.list(startId, size);
+		return Response.ok(entities).build();
+	}
+
+}
