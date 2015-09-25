53a1a833bce5589aaaecd8568a5340c7f23e7a59
fucar <fmucar@cooldatasoft.com>
2015-09-25 16:46:29 +0100

    removing interface

--- tomcat-resteasy/src/main/java/com/cooldatasoft/rest/resource/SampleResource.java
+++ tomcat-resteasy/src/main/java/com/cooldatasoft/rest/resource/SampleResource.java
@@ -1,5 +1,7 @@
 package com.cooldatasoft.rest.resource;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
@@ -14,29 +16,44 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.cooldatasoft.rest.data.SampleEntity;
+import com.cooldatasoft.rest.service.SampleService;
 
+@Named
 @Path("/sample")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public interface SampleResource {
+public class SampleResource {
+
+	@Inject
+	private SampleService sampleService;
 
 	@POST
 	@Path("/create")
-	public Response create(SampleEntity entity);
+	public Response create(SampleEntity entity) {
+		return Response.ok(sampleService.create()).build();
+	}
 
 	@GET
 	@Path("/read/{id}")
-	public Response read(@PathParam("id") Long id);
+	public Response read(@PathParam("id") Long id) {
+		return Response.ok(sampleService.read(id)).build();
+	}
 
 	@PUT
 	@Path("/update")
-	public Response update(SampleEntity entity);
+	public Response update(SampleEntity entity) {
+		return Response.ok(sampleService.update(entity)).build();
+	}
 
 	@DELETE
 	@Path("/delete/{id}")
-	public Response delete(@PathParam("id") Long id);
+	public Response delete(@PathParam("id") Long id) {
+		return Response.ok(sampleService.delete(id)).build();
+	}
 
 	@GET
 	@Path("/list")
-	public Response list(@QueryParam("startId") @DefaultValue("1") Long startId, @QueryParam("size") @DefaultValue("20") Integer size);
+	public Response list(@QueryParam("startId") @DefaultValue("1") Long startId, @QueryParam("size") @DefaultValue("20") Integer size) {
+		return Response.ok(sampleService.list(startId, size)).build();
+	}
 }
